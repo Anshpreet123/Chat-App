b4600f238ad6fb7bf866ef8b44de4c75772079cf
Anshpreet123 <108647469+Anshpreet123@users.noreply.github.com>
2022-12-30 00:12:26 +0530

    first commit

---
+++ js/client.js
@@ -0,0 +1,59 @@
+const socket = io('http://localhost:6999');
+
+const form = document.getElementById('send-container');
+const messageInput = document.getElementById('messageInp');
+const messageContainer = document.querySelector('.container')
+
+var audio = new Audio('css/Ring.mp3');
+
+const append = (message, position)=>{
+    const messageElement = document.createElement('div');
+    messageElement.innerText = message;
+    messageElement.classList.add('message');
+    messageElement.classList.add(position)
+    messageContainer.append(messageElement);
+    messageContainer.scrollTop = messageContainer.scrollHeight;
+    if(position == 'left'){
+        // console.log('sound is playing');
+        audio.play();
+    }
+}
+
+
+form.addEventListener('submit', (e)=>{
+    e.preventDefault();
+    const message = messageInput.value;
+    append(`You: ${message}`, 'right');
+    socket.emit('send', message);
+    messageInput.value = '';
+})
+
+const name = prompt("Enter your name to join LetsChat")
+socket.emit('new-user-joined', name)
+
+socket.on('user-joined', name=>{
+    append(`${name} joined the chat`, 'right');
+})
+
+socket.on('receive', data=>{
+    append(`${data.name }: ${data.message}`, 'left')
+})
+
+
+
+socket.on('left', name=>{
+    append(`${name } left the chat`, 'left');
+})
+
+// for emoji
+const button = document.querySelector('#emoji-button');
+
+const picker = new EmojiButton();
+
+button.addEventListener('click', () => {
+  picker.togglePicker(button);
+});
+
+picker.on('emoji', emoji => {
+    document.querySelector('#messageInp').value += emoji;
+  });
